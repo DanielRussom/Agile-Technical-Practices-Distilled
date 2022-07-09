67e8e4782730445d2ae678a0ea7a3bb19356af2d
Daniel Russom <daniel.russom00@gmail.com>
2022-07-09 22:50:03 +0100

    FizzBuzz : ToString of input

---
+++ Agile Technical Practices Distilled/Agile Technical Practices Distilled Tests/Chapter 2/FizzBuzzConverterShould.cs
@@ -0,0 +1,29 @@
+﻿using Agile_Technical_Practices_Distilled.Chapter_2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Agile_Technical_Practices_Distilled.Tests.Chapter_2
+{
+    [TestClass]
+    public class FizzBuzzConverterShould
+    {
+        private FizzBuzzConverter UnderTest;
+
+        public FizzBuzzConverterShould()
+        {
+            UnderTest = new FizzBuzzConverter();
+        }
+
+
+        [TestMethod]
+        [DataRow (1)]
+        [DataRow (2)]
+        [DataRow (4)]
+        [DataRow (7)]
+        public void Return_string_value_of_input(int input)
+        {
+            var result = UnderTest.Convert(input);
+
+            Assert.AreEqual(result, input.ToString());
+        }
+    }
+}
---
+++ Agile Technical Practices Distilled/Agile Technical Practices Distilled/Chapter 2/FizzBuzzConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Agile_Technical_Practices_Distilled.Chapter_2
+{
+    public class FizzBuzzConverter
+    {
+        public FizzBuzzConverter()
+        {
+        }
+
+        public string Convert(int input)
+        {
+            return input.ToString();
+        }
+    }
+}
