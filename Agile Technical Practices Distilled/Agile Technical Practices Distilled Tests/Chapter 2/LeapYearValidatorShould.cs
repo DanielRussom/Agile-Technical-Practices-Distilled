10083f4b30d0551eea1ca34b905b52db993926ec
Daniel Russom <daniel.russom00@gmail.com>
2022-07-11 23:30:06 +0100

    Leap Year Validation first pass

---
+++ Agile Technical Practices Distilled/Agile Technical Practices Distilled Tests/Chapter 2/LeapYearValidatorShould.cs
@@ -0,0 +1,18 @@
+﻿using Agile_Technical_Practices_Distilled.Chapter_2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Agile_Technical_Practices_Distilled.Tests.Chapter_2
+{
+    [TestClass]
+    public class LeapYearValidatorShould
+    {
+        [TestMethod]
+        public void Return_false()
+        {
+            var UnderTest = new LeapYearValidator();
+
+            var result = UnderTest.Validate(2001);
+            Assert.IsFalse(result);
+        }
+    }
+}
---
+++ Agile Technical Practices Distilled/Agile Technical Practices Distilled/Chapter 2/LeapYearValidator.cs
@@ -0,0 +1,14 @@
+﻿namespace Agile_Technical_Practices_Distilled.Chapter_2
+{
+    public class LeapYearValidator
+    {
+        public LeapYearValidator()
+        {
+        }
+
+        public bool Validate(int input)
+        {
+            return false;
+        }
+    }
+}
