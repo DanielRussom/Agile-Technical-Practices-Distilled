31025c9dd998ca5dc9b3731c751d26e17ccaccaa
Daniel Russom <daniel.russom00@gmail.com>
2022-07-12 23:56:11 +0100

    Fibonacci Generator : 4th number refactor

--- Agile Technical Practices Distilled/Agile Technical Practices Distilled/Chapter 2/FibonacciGenerator.cs
+++ Agile Technical Practices Distilled/Agile Technical Practices Distilled/Chapter 2/FibonacciGenerator.cs
@@ -2,22 +2,33 @@
 {
     public class FibonacciGenerator
     {
+        private int previousNumber = 0;
+        private int currentNumber = 1;
+
         public FibonacciGenerator()
         {
         }
 
         public int Generate(int position)
         {
-            if(position == 4)
+            if (position == 1)
             {
-                return 2;
+                return previousNumber;
             }
 
-            if (position == 2 || position == 3) {
-                return 1;
-            }
+            CalculateNumber(position);
 
-            return 0;
+            return currentNumber;
+        }
+
+        private void CalculateNumber(int position)
+        {
+            for (int i = 3; i <= position; i++)
+            {
+                var newNumber = currentNumber + previousNumber;
+                previousNumber = currentNumber;
+                currentNumber = newNumber;
+            }
         }
     }
 }
