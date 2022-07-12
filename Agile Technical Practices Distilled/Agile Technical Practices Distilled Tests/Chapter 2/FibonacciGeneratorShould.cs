db437fb667510e01b99c41e1e56dca180668df9b
Daniel Russom <daniel.russom00@gmail.com>
2022-07-12 23:57:28 +0100

    Fibonacci Generator : nth number

--- Agile Technical Practices Distilled/Agile Technical Practices Distilled Tests/Chapter 2/FibonacciGeneratorShould.cs
+++ Agile Technical Practices Distilled/Agile Technical Practices Distilled Tests/Chapter 2/FibonacciGeneratorShould.cs
@@ -9,6 +9,8 @@ namespace Agile_Technical_Practices_Distilled.Tests.Chapter_2
         [DataRow (2, 1)]
         [DataRow (3, 1)]
         [DataRow (4, 2)]
+        [DataRow (7, 8)]
+        [DataRow (10, 34)]
         public void Generate_nth_fibonacci_number(int input, int expected)
         {
             var UnderTest = new FibonacciGenerator();
