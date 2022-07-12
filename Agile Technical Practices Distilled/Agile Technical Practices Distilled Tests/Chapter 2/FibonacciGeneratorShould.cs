1201cb4b09b2a81fa9e8f773c33f18515136e4f1
Daniel Russom <daniel.russom00@gmail.com>
2022-07-12 23:37:29 +0100

    Fibonacci Generator : 1st number

---
+++ Agile Technical Practices Distilled/Agile Technical Practices Distilled Tests/Chapter 2/FibonacciGeneratorShould.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace Agile_Technical_Practices_Distilled.Tests.Chapter_2
+{
+    [TestClass]
+    public class FibonacciGeneratorShould
+    {
+        [TestMethod]
+        public void Generate_0()
+        {
+            var UnderTest = new FibonacciGenerator();
+
+            var result = UnderTest.Generate(1);
+
+            Assert.AreEqual(0, result);
+        }
+    }
+}
---
+++ Agile Technical Practices Distilled/Agile Technical Practices Distilled/Chapter 2/FibonacciGenerator.cs
@@ -0,0 +1,14 @@
+﻿namespace Agile_Technical_Practices_Distilled.Tests.Chapter_2
+{
+    public class FibonacciGenerator
+    {
+        public FibonacciGenerator()
+        {
+        }
+
+        public int Generate(int input)
+        {
+            return 0;
+        }
+    }
+}
