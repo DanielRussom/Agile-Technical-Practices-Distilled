2730094a8cff458f9265c63021aec88ed30d0a69
Daniel Russom <daniel.russom00@gmail.com>
2022-07-15 22:41:47 +0100

    Anagram Generator : Init

---
+++ Agile Technical Practices Distilled/Agile Technical Practices Distilled Tests/Chapter 3/AnagramGeneratorShould.cs
@@ -0,0 +1,27 @@
+﻿using Agile_Technical_Practices_Distilled.Chapter_3;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Agile_Technical_Practices_Distilled.Tests.Chapter_3
+{
+    [TestClass]
+    public class AnagramGeneratorShould
+    {
+        private AnagramGenerator UnderTest;
+
+        public AnagramGeneratorShould()
+        {
+            UnderTest = new AnagramGenerator();
+        }
+
+        [TestMethod]
+        public void Generate_a_single_entry_list()
+        {
+            var input = "a";
+
+            var result = UnderTest.Generate(input);
+
+            Assert.AreEqual(1, result.Count);
+            Assert.AreEqual("a", result[0]);
+        }
+    }
+}
---
+++ Agile Technical Practices Distilled/Agile Technical Practices Distilled/Chapter 3/AnagramGenerator.cs
@@ -0,0 +1,14 @@
+﻿namespace Agile_Technical_Practices_Distilled.Chapter_3
+{
+    public class AnagramGenerator
+    {
+        public AnagramGenerator()
+        {
+        }
+
+        public List<string> Generate(string input)
+        {
+            return new List<string> { "a" };
+        }
+    }
+}
