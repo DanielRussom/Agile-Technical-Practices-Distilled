bd2ff995a179176ca7a7d10e1677bab7c7e1a195
Daniel Russom <daniel.russom00@gmail.com>
2022-07-17 16:17:00 +0100

    Roman Numeral Converter V1 : 1000

--- Agile Technical Practices Distilled/Agile Technical Practices Distilled Tests/Chapter 4/RomanNumeralConverterV1Should.cs
+++ Agile Technical Practices Distilled/Agile Technical Practices Distilled Tests/Chapter 4/RomanNumeralConverterV1Should.cs
@@ -18,6 +18,13 @@ namespace Agile_Technical_Practices_Distilled.Tests.Chapter_4
         [DataRow (17, "XVII")]
         [DataRow (36, "XXXVI")]
         [DataRow (49, "XLIX")]
+        [DataRow (50, "L")]
+        [DataRow (90, "XC")]
+        [DataRow (100, "C")]
+        [DataRow (400, "CD")]
+        [DataRow (500, "D")]
+        [DataRow (900, "CM")]
+        [DataRow (1000, "M")]
 
         public void Convert_integer_to_expected_roman_numeral(int input, string expected)
         {
