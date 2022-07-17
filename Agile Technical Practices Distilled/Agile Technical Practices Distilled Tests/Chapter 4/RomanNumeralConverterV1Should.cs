bef200c4a40fbc2ac0952c595dccf1b3d81b843a
Daniel Russom <daniel.russom00@gmail.com>
2022-07-17 15:40:44 +0100

    Roman Numeral Converter V1

---
+++ Agile Technical Practices Distilled/Agile Technical Practices Distilled Tests/Chapter 4/RomanNumeralConverterV1Should.cs
@@ -0,0 +1,19 @@
+﻿using Agile_Technical_Practices_Distilled.Chapter_4;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Agile_Technical_Practices_Distilled.Tests.Chapter_4
+{
+    [TestClass]
+    public class RomanNumeralConverterV1Should
+    {
+        [TestMethod]
+        public void Convert_1_to_I()
+        {
+            var UnderTest = new RomanNumeralConverterV1();
+
+            var result = UnderTest.Convert(1);
+
+            Assert.AreEqual("I", result);
+        }
+    }
+}
