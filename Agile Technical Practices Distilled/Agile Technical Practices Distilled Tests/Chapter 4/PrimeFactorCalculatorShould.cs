6c87f3b9e1ade02d512bcae3914ebe9a6d4e8409
Daniel Russom <daniel.russom00@gmail.com>
2022-07-19 17:35:32 +0100

    Prime Factors Calculator : init

---
+++ Agile Technical Practices Distilled/Agile Technical Practices Distilled Tests/Chapter 4/PrimeFactorCalculatorShould.cs
@@ -0,0 +1,20 @@
+﻿using Agile_Technical_Practices_Distilled.Chapter_4;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Agile_Technical_Practices_Distilled.Tests.Chapter_4
+{
+    [TestClass]
+    public class PrimeFactorCalculatorShould
+    {
+        [TestMethod]
+        public void Convert_1_to_a_list_with_1()
+        {
+            var UnderTest = new PrimeFactorCalculator();
+
+            var result = UnderTest.Calculate(1);
+
+            Assert.AreEqual(1, result.Count);
+            Assert.IsTrue(result.Contains(1));
+        }
+    }
+}
---
+++ Agile Technical Practices Distilled/Agile Technical Practices Distilled/Chapter 4/PrimeFactorCalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace Agile_Technical_Practices_Distilled.Chapter_4
+{
+    public class PrimeFactorCalculator
+    {
+        public IList<int> Calculate(int input)
+        {
+            return new List<int> { 1 };
+        }
+    }
+}
