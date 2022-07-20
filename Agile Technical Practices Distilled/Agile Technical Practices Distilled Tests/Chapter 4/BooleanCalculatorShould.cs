fba708eebd1bca2b53ccc9425d35bec7bb813445
Daniel Russom <daniel.russom00@gmail.com>
2022-07-20 16:14:55 +0100

    Boolean Calculator : init

---
+++ Agile Technical Practices Distilled/Agile Technical Practices Distilled Tests/Chapter 4/BooleanCalculatorShould.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace Agile_Technical_Practices_Distilled.Tests.Chapter_4
+{
+    [TestClass]
+    public class BooleanCalculatorShould
+    {
+        [TestMethod]
+        [DataRow("TRUE", true)]
+        [DataRow("FALSE", false)]
+        public void Calculate_expected_result(string input, bool expected)
+        {
+            var UnderTest = new BooleanCalculator();
+
+            var result = UnderTest.Calculate(input);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ Agile Technical Practices Distilled/Agile Technical Practices Distilled/Chapter 4/BooleanCalculator.cs
@@ -0,0 +1,10 @@
+﻿namespace Agile_Technical_Practices_Distilled.Tests.Chapter_4
+{
+    public class BooleanCalculator
+    {
+        public bool Calculate(string input)
+        {
+            return bool.Parse(input);
+        }
+    }
+}
