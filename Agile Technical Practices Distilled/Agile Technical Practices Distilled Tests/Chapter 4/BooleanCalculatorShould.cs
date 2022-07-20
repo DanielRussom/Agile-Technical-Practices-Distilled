c9dd4ff435ab29e9c0eca4d7e97b471bd0b161d2
Daniel Russom <daniel.russom00@gmail.com>
2022-07-20 16:29:04 +0100

    Boolean Calculator : AND expanded tests

--- Agile Technical Practices Distilled/Agile Technical Practices Distilled Tests/Chapter 4/BooleanCalculatorShould.cs
+++ Agile Technical Practices Distilled/Agile Technical Practices Distilled Tests/Chapter 4/BooleanCalculatorShould.cs
@@ -10,6 +10,9 @@ namespace Agile_Technical_Practices_Distilled.Tests.Chapter_4
         [DataRow("NOT TRUE", false)]
         [DataRow("NOT FALSE", true)]
         [DataRow("TRUE AND TRUE", true)]
+        [DataRow("TRUE AND FALSE", false)]
+        [DataRow("FALSE AND TRUE", false)]
+        [DataRow("FALSE AND FALSE", false)]
         public void Calculate_expected_result(string input, bool expected)
         {
             var UnderTest = new BooleanCalculator();
