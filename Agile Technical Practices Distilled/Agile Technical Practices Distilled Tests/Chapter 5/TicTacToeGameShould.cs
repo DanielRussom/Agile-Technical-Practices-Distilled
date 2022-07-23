c70a5eded08be44878f9028c77ec9a25eef51b37
Daniel Russom <daniel.russom00@gmail.com>
2022-07-24 00:43:29 +0100

    TTT: X center move

--- Agile Technical Practices Distilled/Agile Technical Practices Distilled Tests/Chapter 5/TicTacToeGameShould.cs
+++ Agile Technical Practices Distilled/Agile Technical Practices Distilled Tests/Chapter 5/TicTacToeGameShould.cs
@@ -6,16 +6,33 @@ namespace Agile_Technical_Practices_Distilled.Tests.Chapter_5
     [TestClass]
     public class TicTacToeGameShould
     {
+        TicTacToeGame UnderTest;
+        char[,] ExpectedBoard;
+
+        public TicTacToeGameShould()
+        {
+            UnderTest = new TicTacToeGame();
+
+            ExpectedBoard = new char[3, 3];
+        }
+
         [TestMethod]
         public void Start_with_an_empty_board()
         {
-            var UnderTest = new TicTacToeGame();
-
-            var expected = new char[3,3];
-
             var result = UnderTest.GetBoard();
 
-            CollectionAssert.AreEquivalent(expected, result);
+            CollectionAssert.AreEquivalent(ExpectedBoard, result);
+        }
+
+        [TestMethod]
+        public void Mark_the_middle_position_with_X()
+        {
+            ExpectedBoard[1, 1] = 'X';
+
+            UnderTest.Play(1, 1);
+
+            var result = UnderTest.GetBoard();
+            CollectionAssert.AreEquivalent(ExpectedBoard, result);
         }
     }
 }
--- Agile Technical Practices Distilled/Agile Technical Practices Distilled/Chapter 5/TicTacToeGame.cs
+++ Agile Technical Practices Distilled/Agile Technical Practices Distilled/Chapter 5/TicTacToeGame.cs
@@ -2,13 +2,21 @@
 {
     public class TicTacToeGame
     {
+        private char[,] board;
+
         public TicTacToeGame()
         {
+            board = new char[3, 3];
+        }
+
+        public void Play(int xCoord, int yCoord)
+        {
+            board[1, 1] = 'X';
         }
 
         public char[,] GetBoard()
         {
-            return new char[3, 3];
+            return board;
         }
     }
 }
