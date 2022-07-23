442708abe99dea01beff4d9c64b2cfa6282c0e58
Daniel Russom <daniel.russom00@gmail.com>
2022-07-23 15:59:53 +0100

    Chapter 5 init

---
+++ Agile Technical Practices Distilled/Agile Technical Practices Distilled Tests/Chapter 5/TicTacToeGameShould.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Agile_Technical_Practices_Distilled.Tests.Chapter_5
+{
+    [TestClass]
+    public class TicTacToeGameShould
+    {
+        [TestMethod]
+        public void Start_with_an_empty_board()
+        {
+
+            
+        }
+    }
+}
