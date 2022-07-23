d071c2d5701e5e8a440f88c20cf5c1d6eeb4fa3f
Daniel Russom <daniel.russom00@gmail.com>
2022-07-24 00:16:50 +0100

    TicTacToe : Start with empty board

--- Agile Technical Practices Distilled/Agile Technical Practices Distilled Tests/Chapter 5/TicTacToeGameShould.cs
+++ Agile Technical Practices Distilled/Agile Technical Practices Distilled Tests/Chapter 5/TicTacToeGameShould.cs
@@ -1,6 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
+﻿using Agile_Technical_Practices_Distilled.Chapter_5;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Agile_Technical_Practices_Distilled.Tests.Chapter_5
 {
@@ -10,8 +9,13 @@ namespace Agile_Technical_Practices_Distilled.Tests.Chapter_5
         [TestMethod]
         public void Start_with_an_empty_board()
         {
+            var UnderTest = new TicTacToeGame();
 
-            
+            var expected = new char[3,3];
+
+            var result = UnderTest.GetBoard();
+
+            CollectionAssert.AreEquivalent(expected, result);
         }
     }
 }
---
+++ Agile Technical Practices Distilled/Agile Technical Practices Distilled/Chapter 5/TicTacToeGame.cs
@@ -0,0 +1,14 @@
+﻿namespace Agile_Technical_Practices_Distilled.Chapter_5
+{
+    public class TicTacToeGame
+    {
+        public TicTacToeGame()
+        {
+        }
+
+        public char[,] GetBoard()
+        {
+            return new char[3, 3];
+        }
+    }
+}
