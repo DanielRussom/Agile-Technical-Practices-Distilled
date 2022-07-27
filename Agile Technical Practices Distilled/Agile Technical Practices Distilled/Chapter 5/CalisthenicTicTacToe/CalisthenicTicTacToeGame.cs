a5c73c1325a14baba6c5cd2a5335c4c67fd444a5
Daniel Russom <daniel.russom00@gmail.com>
2022-07-27 22:22:47 +0100

    TTT + Calisthenics : Init Refactor

---
+++ Agile Technical Practices Distilled/Agile Technical Practices Distilled/Chapter 5/CalisthenicTicTacToe/CalisthenicTicTacToeGame.cs
@@ -0,0 +1,17 @@
+﻿namespace Agile_Technical_Practices_Distilled.Chapter_5.CalisthenicTicTacToe
+{
+    public class CalisthenicTicTacToeGame
+    {
+        private GameBoard Board;
+
+        public CalisthenicTicTacToeGame()
+        {
+            Board = new GameBoard();
+        }
+
+        public bool IsBoardEqualTo(List<List<char>> toCompare)
+        {
+            return Board.Equals(toCompare);
+        }
+    }
+}
---
+++ Agile Technical Practices Distilled/Agile Technical Practices Distilled/Chapter 5/CalisthenicTicTacToe/GameBoard.cs
@@ -0,0 +1,36 @@
+﻿namespace Agile_Technical_Practices_Distilled.Chapter_5.CalisthenicTicTacToe
+{
+    public class GameBoard
+    {
+        private List<List<char>> BoardPositions;
+
+        public GameBoard()
+        {
+            BoardPositions = new List<List<char>>
+            {
+                new List<char>(),
+                new List<char>(),
+                new List<char>()
+            };
+        }
+
+
+        public bool Equals(List<List<char>> toCompare)
+        {
+            if (BoardPositions.Count != toCompare.Count)
+            {
+                return false;
+            }
+
+
+            for (int row = 0; row < BoardPositions.Count; row++)
+            {
+                if (!BoardPositions[row].SequenceEqual(toCompare[row])){
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
