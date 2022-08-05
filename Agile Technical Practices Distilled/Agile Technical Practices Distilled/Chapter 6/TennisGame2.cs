9bcd61c0d1b08691403456aa04486b1392f99685
Daniel Russom <daniel.russom00@gmail.com>
2022-08-05 18:48:51 +0100

    TG2 : Remove magic empty strings + format conditionals

--- Agile Technical Practices Distilled/Agile Technical Practices Distilled/Chapter 6/TennisGame2.cs
+++ Agile Technical Practices Distilled/Agile Technical Practices Distilled/Chapter 6/TennisGame2.cs
@@ -5,45 +5,75 @@ namespace Agile_Technical_Practices_Distilled.Chapter_6
         private int p1point = 0;
         private int p2point = 0;
 
-        private string p1res = "";
-        private string p2res = "";
+        private string p1res = string.Empty;
+        private string p2res = string.Empty;
 
         public string GetScore()
         {
-            var score = "";
+            var score = string.Empty;
+
             if (p1point == p2point && p1point < 3)
             {
                 if (p1point == 0)
+                {
                     score = "Love";
+                }
+
                 if (p1point == 1)
+                {
                     score = "Fifteen";
+                }
+
                 if (p1point == 2)
+                {
                     score = "Thirty";
+                }
+
                 score += "-All";
             }
+
             if (p1point == p2point && p1point > 2)
+            {
                 score = "Deuce";
+            }
 
             if (p1point > 0 && p2point == 0)
             {
                 if (p1point == 1)
+                {
                     p1res = "Fifteen";
+                }
+
                 if (p1point == 2)
+                {
                     p1res = "Thirty";
+                }
+
                 if (p1point == 3)
+                {
                     p1res = "Forty";
+                }
 
                 p2res = "Love";
                 score = p1res + "-" + p2res;
             }
+
             if (p2point > 0 && p1point == 0)
             {
                 if (p2point == 1)
+                {
                     p2res = "Fifteen";
+                }
+
                 if (p2point == 2)
+                {
                     p2res = "Thirty";
+                }
+
                 if (p2point == 3)
+                {
                     p2res = "Forty";
+                }
 
                 p1res = "Love";
                 score = p1res + "-" + p2res;
@@ -52,25 +82,49 @@ namespace Agile_Technical_Practices_Distilled.Chapter_6
             if (p1point > p2point && p1point < 4)
             {
                 if (p1point == 2)
+                {
                     p1res = "Thirty";
+                }
+
                 if (p1point == 3)
+                {
                     p1res = "Forty";
+                }
+
                 if (p2point == 1)
+                {
                     p2res = "Fifteen";
+                }
+
                 if (p2point == 2)
+                {
                     p2res = "Thirty";
+                }
+
                 score = p1res + "-" + p2res;
             }
+
             if (p2point > p1point && p2point < 4)
             {
                 if (p2point == 2)
+                {
                     p2res = "Thirty";
+                }
+
                 if (p2point == 3)
+                {
                     p2res = "Forty";
+                }
+
                 if (p1point == 1)
+                {
                     p1res = "Fifteen";
+                }
                 if (p1point == 2)
+                {
                     p1res = "Thirty";
+                }
+
                 score = p1res + "-" + p2res;
             }
 
@@ -88,10 +142,12 @@ namespace Agile_Technical_Practices_Distilled.Chapter_6
             {
                 score = "Win for player1";
             }
+
             if (p2point >= 4 && p1point >= 0 && (p2point - p1point) >= 2)
             {
                 score = "Win for player2";
             }
+
             return score;
         }
 
@@ -124,9 +180,13 @@ namespace Agile_Technical_Practices_Distilled.Chapter_6
         public void WonPoint(string player)
         {
             if (player == "player1")
+            {
                 P1Score();
+            }
             else
+            {
                 P2Score();
+            }
         }
 
     }
