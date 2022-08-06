3fd213d9ef63b4ce06a42f2b7bb6a0bea0e35d4c
Daniel Russom <daniel.russom00@gmail.com>
2022-08-06 16:28:26 +0100

    TG2: Extract Matching score conditionals

--- Agile Technical Practices Distilled/Agile Technical Practices Distilled/Chapter 6/TennisGame2.cs
+++ Agile Technical Practices Distilled/Agile Technical Practices Distilled/Chapter 6/TennisGame2.cs
@@ -10,14 +10,14 @@ namespace Agile_Technical_Practices_Distilled.Chapter_6
 
         public string GetScore()
         {
-            if (playerOnePoints == playerTwoPoints && playerOnePoints < 3)
+            if (PlayerScoresMatch() && PlayerOneScoreIsUnderThree())
             {
                 var playerScoreText = GetScoreText(playerOnePoints);
 
                 return $"{playerScoreText}-All";
             }
 
-            if (playerOnePoints == playerTwoPoints && playerOnePoints > 2)
+            if (PlayerScoresMatch())
             {
                 return "Deuce";
             }
@@ -53,6 +53,16 @@ namespace Agile_Technical_Practices_Distilled.Chapter_6
             }
 
             return string.Empty;
+        }
+
+        private bool PlayerOneScoreIsUnderThree()
+        {
+            return playerOnePoints < 3;
+        }
+
+        private bool PlayerScoresMatch()
+        {
+            return playerOnePoints == playerTwoPoints;
         }
 
         private int GetDifferenceInPlayerScores()
