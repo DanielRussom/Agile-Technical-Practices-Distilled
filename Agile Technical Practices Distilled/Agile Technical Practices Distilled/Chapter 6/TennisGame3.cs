0ea707c312be346565410e30949674bab064aa44
Daniel Russom <daniel.russom00@gmail.com>
2022-08-07 21:15:45 +0100

    TG3: s variable - remove unrequired else

--- Agile Technical Practices Distilled/Agile Technical Practices Distilled/Chapter 6/TennisGame3.cs
+++ Agile Technical Practices Distilled/Agile Technical Practices Distilled/Chapter 6/TennisGame3.cs
@@ -15,20 +15,18 @@ namespace Agile_Technical_Practices_Distilled.Chapter_6
 
         public string GetScore()
         {
-            string s;
+            var scoreMessage = string.Empty;
             if ((playerOneScore < 4 && playerTwoScore < 4) && (playerOneScore + playerTwoScore < 6))
             {
                 string[] p = { "Love", "Fifteen", "Thirty", "Forty" };
-                s = p[playerOneScore];
-                return (playerOneScore == playerTwoScore) ? s + "-All" : s + "-" + p[playerTwoScore];
+                scoreMessage = p[playerOneScore];
+                return (playerOneScore == playerTwoScore) ? scoreMessage + "-All" : scoreMessage + "-" + p[playerTwoScore];
             }
-            else
-            {
-                if (playerOneScore == playerTwoScore)
-                    return "Deuce";
-                s = playerOneScore > playerTwoScore ? playerOneName : playerTwoName;
-                return ((playerOneScore - playerTwoScore) * (playerOneScore - playerTwoScore) == 1) ? "Advantage " + s : "Win for " + s;
-            }
+         
+            if (playerOneScore == playerTwoScore)
+                return "Deuce";
+            scoreMessage = playerOneScore > playerTwoScore ? playerOneName : playerTwoName;
+            return ((playerOneScore - playerTwoScore) * (playerOneScore - playerTwoScore) == 1) ? "Advantage " + scoreMessage : "Win for " + scoreMessage;
         }
 
         public void WonPoint(string playerName)
