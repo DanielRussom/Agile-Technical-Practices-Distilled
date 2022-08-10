ff7c3feb38553fdd00b364b8326fb48f9545a8d0
Daniel Russom <daniel.russom00@gmail.com>
2022-08-10 19:26:53 +0100

    C7: Use for loop to tody Win check

--- Agile Technical Practices Distilled/Agile Technical Practices Distilled/Chapter 7/Game.cs
+++ Agile Technical Practices Distilled/Agile Technical Practices Distilled/Chapter 7/Game.cs
@@ -39,50 +39,23 @@
         }
 
         public char Winner()
-        {   //if the positions in first row are taken
-            if(_board.TileAt(0, 0).Symbol != ' ' &&
-               _board.TileAt(0, 1).Symbol != ' ' &&
-               _board.TileAt(0, 2).Symbol != ' ')
-               {
+        {
+            for (int row = 0; row < 3; row++)
+            {
+                if (_board.TileAt(row, 0).Symbol != ' ' &&
+               _board.TileAt(row, 1).Symbol != ' ' &&
+               _board.TileAt(row, 2).Symbol != ' ')
+                {
                     //if first row is full with same symbol
-                    if (_board.TileAt(0, 0).Symbol == 
-                        _board.TileAt(0, 1).Symbol &&
-                        _board.TileAt(0, 2).Symbol == 
-                        _board.TileAt(0, 1).Symbol )
-                        {
-                            return _board.TileAt(0, 0).Symbol;
-                        }
-               }
-                
-             //if the positions in first row are taken
-             if(_board.TileAt(1, 0).Symbol != ' ' &&
-                _board.TileAt(1, 1).Symbol != ' ' &&
-                _board.TileAt(1, 2).Symbol != ' ')
-                {
-                    //if middle row is full with same symbol
-                    if (_board.TileAt(1, 0).Symbol == 
-                        _board.TileAt(1, 1).Symbol &&
-                        _board.TileAt(1, 2).Symbol == 
-                        _board.TileAt(1, 1).Symbol)
-                        {
-                            return _board.TileAt(1, 0).Symbol;
-                        }
+                    if (_board.TileAt(row, 0).Symbol ==
+                        _board.TileAt(row, 1).Symbol &&
+                        _board.TileAt(row, 2).Symbol ==
+                        _board.TileAt(row, 1).Symbol)
+                    {
+                        return _board.TileAt(row, 0).Symbol;
+                    }
                 }
-
-            //if the positions in first row are taken
-             if(_board.TileAt(2, 0).Symbol != ' ' &&
-                _board.TileAt(2, 1).Symbol != ' ' &&
-                _board.TileAt(2, 2).Symbol != ' ')
-                {
-                    //if middle row is full with same symbol
-                    if (_board.TileAt(2, 0).Symbol == 
-                        _board.TileAt(2, 1).Symbol &&
-                        _board.TileAt(2, 2).Symbol == 
-                        _board.TileAt(2, 1).Symbol)
-                        {
-                            return _board.TileAt(2, 0).Symbol;
-                        }
-                }
+            }
 
             return ' ';
         }
