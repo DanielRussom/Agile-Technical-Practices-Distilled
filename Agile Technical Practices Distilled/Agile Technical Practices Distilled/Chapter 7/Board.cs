a90ae211f4aa1adec9d2a6c552beebb3707f4505
Daniel Russom <daniel.russom00@gmail.com>
2022-08-10 16:27:58 +0100

    C7: Move classes into seperate files

---
+++ Agile Technical Practices Distilled/Agile Technical Practices Distilled/Chapter 7/Board.cs
@@ -0,0 +1,35 @@
+﻿namespace Agile_Technical_Practices_Distilled.Chapter_7
+{
+    public class Board
+    {
+       private List<Tile> _plays = new();
+       
+        public Board()
+        {
+            for (int xCoord = 0; xCoord < 3; xCoord++)
+            {
+                for (int yCoord = 0; yCoord < 3; yCoord++)
+                {
+                    _plays.Add(new Tile{ XPosition = xCoord, YPosition = yCoord, Symbol = ' '});
+                }  
+            }       
+        }
+
+       public Tile TileAt(int x, int y)
+       {
+           return _plays.Single(tile => tile.XPosition == x && tile.YPosition == y);
+       }
+
+       public void AddTileAt(char symbol, int x, int y)
+       {
+           var newTile = new Tile
+           {
+               XPosition = x,
+               YPosition = y,
+               Symbol = symbol
+           };
+
+           _plays.Single(tile => tile.XPosition == x && tile.YPosition == y).Symbol = symbol;
+       }
+    }
+}
---
+++ Agile Technical Practices Distilled/Agile Technical Practices Distilled/Chapter 7/Tile.cs
@@ -0,0 +1,11 @@
+﻿namespace Agile_Technical_Practices_Distilled.Chapter_7
+{
+    public class Tile
+    {
+        public int XPosition {get; set;}
+
+        public int YPosition {get; set;}
+
+        public char Symbol {get; set;}
+    }
+}
