49d16bfbe5420bf104c0368b95ec27b96399f234
Daniel Russom <daniel.russom00@gmail.com>
2022-08-17 19:31:08 +0100

    GR : Add 50 quality limit test

--- Agile Technical Practices Distilled/Agile Technical Practices Distilled Tests/Chapter 9/GildedRoseShould.cs
+++ Agile Technical Practices Distilled/Agile Technical Practices Distilled Tests/Chapter 9/GildedRoseShould.cs
@@ -43,6 +43,18 @@ namespace Agile_Technical_Practices_Distilled.Tests.Chapter_9
             Assert.AreEqual(7, resultItem.Quality);
         }
 
+        [TestMethod]
+        public void Not_increase_aged_brie_quality_past_50()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 5, Quality = 50 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            var resultItem = Items[0];
+            Assert.AreEqual("Aged Brie", resultItem.Name);
+            Assert.AreEqual(4, resultItem.SellIn);
+            Assert.AreEqual(50, resultItem.Quality);
+        }
+
         //Unique cases: Aged Brie, Backstage passes to a TAFKAL80ETC concert, Sulfuras, Hand of Ragnaros
     }
 }
