b9b92364219bf043da40e721790d4f2440b0c21f
Daniel Russom <daniel.russom00@gmail.com>
2022-08-17 19:30:19 +0100

    GR : Add Brie tests

--- Agile Technical Practices Distilled/Agile Technical Practices Distilled Tests/Chapter 9/GildedRoseShould.cs
+++ Agile Technical Practices Distilled/Agile Technical Practices Distilled Tests/Chapter 9/GildedRoseShould.cs
@@ -19,6 +19,30 @@ namespace Agile_Technical_Practices_Distilled.Tests.Chapter_9
             Assert.AreEqual(0, resultItem.Quality);
         }
 
+        [TestMethod]
+        public void Increase_aged_brie_quality()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            var resultItem = Items[0];
+            Assert.AreEqual("Aged Brie", resultItem.Name);
+            Assert.AreEqual(1, resultItem.SellIn);
+            Assert.AreEqual(1, resultItem.Quality);
+        }
+
+        [TestMethod]
+        public void Increase_aged_brie_quality_twice_as_fast_after_sell_in_becomes_negative()
+        {
+            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 0, Quality = 5 } };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            var resultItem = Items[0];
+            Assert.AreEqual("Aged Brie", resultItem.Name);
+            Assert.AreEqual(-1, resultItem.SellIn);
+            Assert.AreEqual(7, resultItem.Quality);
+        }
+
         //Unique cases: Aged Brie, Backstage passes to a TAFKAL80ETC concert, Sulfuras, Hand of Ragnaros
     }
 }
