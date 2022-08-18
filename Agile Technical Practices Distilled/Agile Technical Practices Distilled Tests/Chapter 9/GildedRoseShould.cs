f900038cccf7b53421f6502abb50e323c5bbdb06
Daniel Russom <daniel.russom00@gmail.com>
2022-08-18 17:08:00 +0100

    GR : Double quality loss

--- Agile Technical Practices Distilled/Agile Technical Practices Distilled Tests/Chapter 9/GildedRoseShould.cs
+++ Agile Technical Practices Distilled/Agile Technical Practices Distilled Tests/Chapter 9/GildedRoseShould.cs
@@ -8,24 +8,42 @@ namespace Agile_Technical_Practices_Distilled.Tests.Chapter_9
     public class GildedRoseShould
     {
         [TestMethod]
-        public void Lower_foo_SellIn()
+        public void Lower_sell_in_value_and_quality_of_default_object()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-            var resultItem = Items[0];
+            var testItems = new List<Item> { new Item { Name = "foo", SellIn = 10, Quality = 10 } };
+            var UnderTest = new GildedRose(testItems);
+
+            UnderTest.UpdateQuality();
+            
+            var resultItem = testItems[0];
+            Assert.AreEqual("foo", resultItem.Name);
+            Assert.AreEqual(9, resultItem.SellIn);
+            Assert.AreEqual(9, resultItem.Quality);
+        }
+
+        [TestMethod]
+        public void Lower_quality_by_two_for_default_object_with_negative_sell_in()
+        {
+            var testItems = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 10 } };
+            var UnderTest = new GildedRose(testItems);
+
+            UnderTest.UpdateQuality();
+
+            var resultItem = testItems[0];
             Assert.AreEqual("foo", resultItem.Name);
             Assert.AreEqual(-1, resultItem.SellIn);
-            Assert.AreEqual(0, resultItem.Quality);
+            Assert.AreEqual(8, resultItem.Quality);
         }
 
         [TestMethod]
         public void Increase_aged_brie_quality()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 } };
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-            var resultItem = Items[0];
+            var testItems = new List<Item> { new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 } };
+            var UnderTest = new GildedRose(testItems);
+
+            UnderTest.UpdateQuality();
+
+            var resultItem = testItems[0];
             Assert.AreEqual("Aged Brie", resultItem.Name);
             Assert.AreEqual(1, resultItem.SellIn);
             Assert.AreEqual(1, resultItem.Quality);
@@ -34,10 +52,12 @@ namespace Agile_Technical_Practices_Distilled.Tests.Chapter_9
         [TestMethod]
         public void Increase_aged_brie_quality_twice_as_fast_after_sell_in_becomes_negative()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 0, Quality = 5 } };
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-            var resultItem = Items[0];
+            var testItems = new List<Item> { new Item { Name = "Aged Brie", SellIn = 0, Quality = 5 } };
+            var UnderTest = new GildedRose(testItems);
+
+            UnderTest.UpdateQuality();
+
+            var resultItem = testItems[0];
             Assert.AreEqual("Aged Brie", resultItem.Name);
             Assert.AreEqual(-1, resultItem.SellIn);
             Assert.AreEqual(7, resultItem.Quality);
@@ -46,10 +66,12 @@ namespace Agile_Technical_Practices_Distilled.Tests.Chapter_9
         [TestMethod]
         public void Not_increase_aged_brie_quality_past_50()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "Aged Brie", SellIn = 5, Quality = 50 } };
-            GildedRose app = new GildedRose(Items);
-            app.UpdateQuality();
-            var resultItem = Items[0];
+            var testItems = new List<Item> { new Item { Name = "Aged Brie", SellIn = 5, Quality = 50 } };
+            var UnderTest = new GildedRose(testItems);
+
+            UnderTest.UpdateQuality();
+
+            var resultItem = testItems[0];
             Assert.AreEqual("Aged Brie", resultItem.Name);
             Assert.AreEqual(4, resultItem.SellIn);
             Assert.AreEqual(50, resultItem.Quality);
