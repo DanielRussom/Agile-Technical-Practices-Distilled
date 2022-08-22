8beb17d65b75474ab06668b371cd3e38939e43e8
Daniel Russom <daniel.russom00@gmail.com>
2022-08-22 09:51:54 +0100

    GR: Tidy tests - use parameterisation for all single item tests

--- Agile Technical Practices Distilled/Agile Technical Practices Distilled Tests/Chapter 9/GildedRoseShould.cs
+++ Agile Technical Practices Distilled/Agile Technical Practices Distilled Tests/Chapter 9/GildedRoseShould.cs
@@ -8,156 +8,29 @@ namespace Agile_Technical_Practices_Distilled.Tests.Chapter_9
     public class GildedRoseShould
     {
         [TestMethod]
-        public void Lower_sell_in_value_and_quality_of_default_object()
+        [DataRow("foo", 10, 10, 9, 9)]
+        [DataRow("foo", 0, 10, -1, 8)]
+        [DataRow("Aged Brie", 2, 0, 1, 1)]
+        [DataRow("Aged Brie", 0, 5, -1, 7)]
+        [DataRow("Aged Brie", 5, 50, 4, 50)]
+        [DataRow("Sulfuras, Hand of Ragnaros", 5, 10, 5, 10)]
+        [DataRow("Backstage passes to a TAFKAL80ETC concert", 0, 25, -1, 0)]
+        [DataRow("Backstage passes to a TAFKAL80ETC concert", 15, 25, 14, 26)]
+        [DataRow("Backstage passes to a TAFKAL80ETC concert", 10, 25, 9, 27)]
+        [DataRow("Backstage passes to a TAFKAL80ETC concert", 5, 48, 4, 50)]
+        [DataRow("Backstage passes to a TAFKAL80ETC concert", 5, 25, 4, 28)]
+        public void Update_sellIn_and_quality_as_expected
+            (string name, int initialSellIn, int initialQuality, int expectedSellIn, int expectedQuality)
         {
-            var testItems = new List<Item> { new Item { Name = "foo", SellIn = 10, Quality = 10 } };
+            var testItems = new List<Item> { new Item { Name = name, SellIn = initialSellIn, Quality = initialQuality } };
             var UnderTest = new GildedRose(testItems);
 
             UnderTest.UpdateQuality();
             
             var resultItem = testItems[0];
-            Assert.AreEqual("foo", resultItem.Name);
-            Assert.AreEqual(9, resultItem.SellIn);
-            Assert.AreEqual(9, resultItem.Quality);
-        }
-
-        [TestMethod]
-        public void Lower_quality_by_two_for_default_object_with_negative_sell_in()
-        {
-            var testItems = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 10 } };
-            var UnderTest = new GildedRose(testItems);
-
-            UnderTest.UpdateQuality();
-
-            var resultItem = testItems[0];
-            Assert.AreEqual(-1, resultItem.SellIn);
-            Assert.AreEqual(8, resultItem.Quality);
-        }
-
-        [TestMethod]
-        public void Increase_aged_brie_quality()
-        {
-            var testItems = new List<Item> { new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 } };
-            var UnderTest = new GildedRose(testItems);
-
-            UnderTest.UpdateQuality();
-
-            var resultItem = testItems[0];
-            Assert.AreEqual("Aged Brie", resultItem.Name);
-            Assert.AreEqual(1, resultItem.SellIn);
-            Assert.AreEqual(1, resultItem.Quality);
-        }
-
-        [TestMethod]
-        public void Increase_aged_brie_quality_twice_as_fast_after_sell_in_becomes_negative()
-        {
-            var testItems = new List<Item> { new Item { Name = "Aged Brie", SellIn = 0, Quality = 5 } };
-            var UnderTest = new GildedRose(testItems);
-
-            UnderTest.UpdateQuality();
-
-            var resultItem = testItems[0];
-            Assert.AreEqual("Aged Brie", resultItem.Name);
-            Assert.AreEqual(-1, resultItem.SellIn);
-            Assert.AreEqual(7, resultItem.Quality);
-        }
-
-        [TestMethod]
-        public void Not_increase_aged_brie_quality_past_50()
-        {
-            var testItems = new List<Item> { new Item { Name = "Aged Brie", SellIn = 5, Quality = 50 } };
-            var UnderTest = new GildedRose(testItems);
-
-            UnderTest.UpdateQuality();
-
-            var resultItem = testItems[0];
-            Assert.AreEqual("Aged Brie", resultItem.Name);
-            Assert.AreEqual(4, resultItem.SellIn);
-            Assert.AreEqual(50, resultItem.Quality);
-        }
-
-        [TestMethod]
-        public void Not_decrease_values_of_sulfuras()
-        {
-            var testItems = new List<Item> { new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 5, Quality = 10 } };
-            var UnderTest = new GildedRose(testItems);
-
-            UnderTest.UpdateQuality();
-
-            var resultItem = testItems[0];
-            Assert.AreEqual("Sulfuras, Hand of Ragnaros", resultItem.Name);
-            Assert.AreEqual(5, resultItem.SellIn);
-            Assert.AreEqual(10, resultItem.Quality);
-        }
-
-        [TestMethod]
-        public void Set_value_of_tickets_with_negative_sell_to_0()
-        {
-            var testItems = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 0, Quality = 25 } };
-            var UnderTest = new GildedRose(testItems);
-
-            UnderTest.UpdateQuality();
-
-            var resultItem = testItems[0];
-            Assert.AreEqual("Backstage passes to a TAFKAL80ETC concert", resultItem.Name);
-            Assert.AreEqual(-1, resultItem.SellIn);
-            Assert.AreEqual(0, resultItem.Quality);
-        }
-
-        [TestMethod]
-        public void Increase_value_of_tickets_by_one()
-        {
-            var testItems = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 25 } };
-            var UnderTest = new GildedRose(testItems);
-
-            UnderTest.UpdateQuality();
-
-            var resultItem = testItems[0];
-            Assert.AreEqual("Backstage passes to a TAFKAL80ETC concert", resultItem.Name);
-            Assert.AreEqual(14, resultItem.SellIn);
-            Assert.AreEqual(26, resultItem.Quality);
-        }
-
-        [TestMethod]
-        public void Increase_value_of_tickets_by_two()
-        {
-            var testItems = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 25 } };
-            var UnderTest = new GildedRose(testItems);
-
-            UnderTest.UpdateQuality();
-
-            var resultItem = testItems[0];
-            Assert.AreEqual("Backstage passes to a TAFKAL80ETC concert", resultItem.Name);
-            Assert.AreEqual(9, resultItem.SellIn);
-            Assert.AreEqual(27, resultItem.Quality);
-        }
-
-        [TestMethod]
-        public void Increase_value_of_tickets_by_two_due_to_50_limit()
-        {
-            var testItems = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 48 } };
-            var UnderTest = new GildedRose(testItems);
-
-            UnderTest.UpdateQuality();
-
-            var resultItem = testItems[0];
-            Assert.AreEqual("Backstage passes to a TAFKAL80ETC concert", resultItem.Name);
-            Assert.AreEqual(4, resultItem.SellIn);
-            Assert.AreEqual(50, resultItem.Quality);
-        }
-
-        [TestMethod]
-        public void Increase_value_of_tickets_by_three()
-        {
-            var testItems = new List<Item> { new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 25 } };
-            var UnderTest = new GildedRose(testItems);
-
-            UnderTest.UpdateQuality();
-
-            var resultItem = testItems[0];
-            Assert.AreEqual("Backstage passes to a TAFKAL80ETC concert", resultItem.Name);
-            Assert.AreEqual(4, resultItem.SellIn);
-            Assert.AreEqual(28, resultItem.Quality);
+            Assert.AreEqual(name, resultItem.Name);
+            Assert.AreEqual(expectedSellIn, resultItem.SellIn);
+            Assert.AreEqual(expectedQuality, resultItem.Quality);
         }
 
         [TestMethod]
