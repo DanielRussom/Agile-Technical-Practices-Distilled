ff0d621314272006d5a95cd64f829c4041254b55
Daniel Russom <daniel.russom00@gmail.com>
2022-08-23 23:13:34 +0100

    init Game of life

---
+++ Agile Technical Practices Distilled/Agile Technical Practices Distilled Tests/Chapter 5/GameOfLife/GameOfLifeShould.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Agile_Technical_Practices_Distilled.Tests.Chapter_5.GameOfLife
+{
+    [TestClass]
+    public class GameOfLifeShould
+    {
+    }
+}
