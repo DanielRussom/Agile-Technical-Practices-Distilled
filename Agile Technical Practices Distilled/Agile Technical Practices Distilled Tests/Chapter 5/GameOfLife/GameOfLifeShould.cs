b46a63600ef022b64a446d3ac167f3ab821a18d5
Daniel Russom <daniel.russom00@gmail.com>
2022-08-29 15:24:42 +0100

    GoL: Toggle 1,0

--- Agile Technical Practices Distilled/Agile Technical Practices Distilled Tests/Chapter 5/GameOfLife/GameOfLifeShould.cs
+++ Agile Technical Practices Distilled/Agile Technical Practices Distilled Tests/Chapter 5/GameOfLife/GameOfLifeShould.cs
@@ -15,11 +15,49 @@ namespace Agile_Technical_Practices_Distilled.Tests.Chapter_5.GameOfLife
                 { 0, 0, 0 },
                 { 0, 0, 0 }
             };
-            var displayer = new Mock<ILifeBoardDisplayer>();
             int[,]? result = null;
+            var displayer = new Mock<ILifeBoardDisplayer>();
             displayer.Setup(x => x.DisplayBoard(It.IsAny<int[,]>())).Callback<int[,]>(r => result = r);
-
             var underTest = new LifeBoard(displayer.Object);
+
+            underTest.DisplayBoard();
+
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Set_position_1_1_to_alive()
+        {
+            var expected = new int[,]{
+                { 0, 0, 0 },
+                { 0, 1, 0 },
+                { 0, 0, 0 }
+            };
+            int[,]? result = null;
+            var displayer = new Mock<ILifeBoardDisplayer>();
+            displayer.Setup(x => x.DisplayBoard(It.IsAny<int[,]>())).Callback<int[,]>(r => result = r);
+            var underTest = new LifeBoard(displayer.Object);
+
+            underTest.ToggleCell(1, 1);
+            underTest.DisplayBoard();
+
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Set_position_0_1_to_alive()
+        {
+            var expected = new int[,]{
+                { 0, 0, 0 },
+                { 1, 0, 0 },
+                { 0, 0, 0 }
+            };
+            int[,]? result = null;
+            var displayer = new Mock<ILifeBoardDisplayer>();
+            displayer.Setup(x => x.DisplayBoard(It.IsAny<int[,]>())).Callback<int[,]>(r => result = r);
+            var underTest = new LifeBoard(displayer.Object);
+
+            underTest.ToggleCell(1, 0);
             underTest.DisplayBoard();
 
             CollectionAssert.AreEqual(expected, result);
--- Agile Technical Practices Distilled/Agile Technical Practices Distilled/Chapter 5/GameOfLife/LifeBoard.cs
+++ Agile Technical Practices Distilled/Agile Technical Practices Distilled/Chapter 5/GameOfLife/LifeBoard.cs
@@ -3,15 +3,22 @@
     public class LifeBoard
     {
         private readonly ILifeBoardDisplayer displayer;
+        private int[,] gameBoard;
 
         public LifeBoard(ILifeBoardDisplayer displayer)
         {
             this.displayer = displayer;
+            gameBoard = new int[3, 3];
         }
 
         public void DisplayBoard()
         {
-            displayer.DisplayBoard(new int[3, 3]);
+            displayer.DisplayBoard(gameBoard);
+        }
+
+        public void ToggleCell(int xCoord, int yCoord)
+        {
+            gameBoard[xCoord, yCoord] = 1;
         }
     }
 }
